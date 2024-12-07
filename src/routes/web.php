3affef54b434fef860a54164ea1b9d7470a78c87
kawaguchi ryosuke <ryosuke.com.jp@gmail.com>
2024-12-07 12:37:18 +0900

    コードの修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ReserveController;
-use App\Http\Controllers\MailController;
 
 Route::controller(ShopController::class)->group(function(){
     Route::get('/', 'index');
